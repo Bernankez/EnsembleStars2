3b43f36669a9d509a8cfb7311e4b9b1b2913098d
cole <bernankez@qq.com>
2022-05-04 14:16:44 +0800

    feat: 黑暗模式支持

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
-import { ConfigProvider } from "vant";
+import { ConfigProvider as VanConfigProvider } from "vant";
 import { storeToRefs } from "pinia";
 import { useAppStore } from "@/store";
+import { watchEffect } from "vue";
 
 const appStore = useAppStore();
 
-const { theme, vantThemeStyle } = storeToRefs(appStore);
+const { theme, vantBaseStyle, vantThemeStyle } = storeToRefs(appStore);
+const { toggleTheme } = appStore;
+
+watchEffect(() => {
+  console.log("set vant base style");
+  document.body.setAttribute("style", JSON.stringify(vantBaseStyle.value));
+});
+
+const media = window.matchMedia("(prefers-color-scheme: dark)");
+const listenTheme = (e: MediaQueryListEvent | MediaQueryList) => {
+  if (e.matches) {
+    console.log("dark mode");
+    toggleTheme("dark");
+  } else {
+    console.log("light mode");
+    toggleTheme("light");
+  }
+};
+listenTheme(media);
+media.addEventListener("change", listenTheme);
 </script>
 
 <template>
